106c75e3f7c3e81ba02c6c8c7fb2a7b4d97d110f
Dawan Sawyer <dawansawyer@gmail.com>
2021-04-23 19:34:10 -0400

    created routes for saving and deleting the note to the db

---
+++ routes/apiroutes.js
@@ -0,0 +1,63 @@
+  
+const fs = require("fs");
+const { parse } = require("path");
+
+module.exports =  (app) => {
+
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+            noteData = JSON.parse(data);
+            res.send(noteData);
+        });
+    });
+
+
+    app.post("/api/notes", (req, res) => {
+        const userNotes = req.body;
+
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+            noteData = JSON.parse(data);
+            noteData.push(userNotes);
+            let number = 1;
+            noteData.forEach((note, index) => {
+                note.id = number;
+                number++;
+                return noteData;
+            });
+            console.log(noteData);
+
+            stringData = JSON.stringify(noteData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        res.send("Thank you for making a note!");
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+
+        const deleteNote = req.params.id;
+        console.log(deleteNote);
+
+        fs.readFile("./db/db.json", (err, data) => {
+            if (err) throw err;
+
+            noteData = JSON.parse(data);
+            for (let i = 0; i < noteData.length; i++) {
+                if (noteData[i].id === Number(deleteNote)) {
+                    noteData.splice([i], 1);
+                }
+            }
+            console.log(noteData);
+            stringData = JSON.stringify(noteData);
+
+            fs.writeFile("./db/db.json", stringData, (err, data) => {
+                if (err) throw err;
+            });
+        });
+        res.status(204).send();
+    });
+};
